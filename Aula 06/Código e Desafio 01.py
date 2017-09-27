249c8cfc4577426566ef9968daffc5a4cb975cd2
LeandroMontanari <otter.lontra@hotmail.com>
2017-09-27 05:47:57 -0300

    Adição dos Arquivos da Aula 06

---
+++ Aula 06/Código e Desafio 01.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+re = n1 + n2
+print('A soma entre', n1, 'e', n2, 'vale', re) # Método 1 - Antigo, do Python 2
+print('A soma entre {} e {} vale {}'.format(n1, n2, re)) # Método 2
+print('A soma entre {0} e {1} vale {2}'.format(n1, n2, re)) # Método 3
---
+++ Aula 06/Código e Desafio 02.py
@@ -0,0 +1,6 @@
+n = input('Digite algo: ')
+print(n.isnumeric()) # Verifica se o que foi digitado é ou pode se tornar um valor numérico; retorna True se sim
+print(n.isalpha()) # Verifica se o que foi digitado é uma letra ou contém apenas letras; retorna True se sim
+print(n.isalnum()) # Verifica se o que foi digitado contém apenas letras e números; retorna True se sim
+print(n.isupper()) # Verifica se o que foi digitado contém apenas letras maiúsculas; retorna True se sim
+# Existem ainda outros métodos "is", como "islower()", "isspace()" etc.
