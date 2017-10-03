f5969449e5a45c8ebf692e872b3d1d2c56f5a3a1
LeandroMontanari <otter.lontra@hotmail.com>
2017-10-03 18:22:23 -0300

    Correção no Desafio 011

--- Mundo 01/Anotações das Aulas e Desafios/Aula 07/Desafio 011.py
+++ Mundo 01/Anotações das Aulas e Desafios/Aula 07/Desafio 011.py
@@ -3,4 +3,5 @@ a = float(input('Digite a altura da parede, em metros: '))
 t = 2
 m2 = l * a
 r = m2 / t
-print('\nA área das paredes de {:.2f}m x {:.2f}m é {:.2f}m², e será necessário comprar {:.2f} litro(s) de tinta para pintar tudo!'.format(l, a, m2, r))
+print('\nA área das paredes de {:.2f}m x {:.2f}m é {:.2f}m², e será necessário comprar'.format(l, a, m2), end=' ')
+print('{:.2f} litro(s) de tinta para pintar tudo!'.format(r))
