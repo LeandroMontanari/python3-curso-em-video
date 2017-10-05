9464a26f37878c3a460a8674edba624f4de69ef7
LeandroMontanari <otter.lontra@hotmail.com>
2017-10-05 20:05:53 -0300

    Adição do Exercício 038

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 038.py
@@ -0,0 +1,8 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segunda número: '))
+if n1 > n2:
+    print('O PRIMEIRO valor é maior!')
+elif n2 > n1:
+    print('O SEGUNDO valor é maior!')
+else:
+    print('Os dois valores são IGUAIS!')
