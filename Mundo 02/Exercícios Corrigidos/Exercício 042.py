b457101742f80da2c473cb7d7f74a3eea266f2c1
LeandroMontanari <otter.lontra@hotmail.com>
2017-10-12 00:08:14 -0300

    Adição do Exercício 042

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 042.py
@@ -0,0 +1,22 @@
+"""
+EXERCÍCIO 042: Analisando Triângulos v2.0
+
+Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+
+- Equilátero: todos os lados iguais
+- Isósceles: dois lados iguais
+- Escaleno: todos os lados diferentes
+"""
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR um triângulo ', end='')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO!')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO!')
+    else:
+        print('ISÓSCELES!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo!')
