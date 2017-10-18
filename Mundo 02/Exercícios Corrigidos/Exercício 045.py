345511a85d1f5732c7733dd22a2e51e4a82a7e2d
LeandroMontanari <otter.lontra@hotmail.com>
2017-10-18 20:05:23 -0200

    Adição do Exercício 045

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 045.py
@@ -0,0 +1,50 @@
+"""
+EXERCÍCIO 045: Pedra, Papel e Tesoura
+
+Crie um programa que faça o computador jogar Jokenpô com você.
+"""
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Sua opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ!!!')
+print('-=' * 12)
+print('Computador jogou {}.'.format(itens[computador]))
+print('Jogador jogou {}.'.format(itens[jogador]))
+print('-=' * 12)
+if computador == 0:  # Computador jogou PEDRA
+    if jogador == 0:
+        print('EMPATE!')
+    elif jogador == 1:
+        print('JOGADOR VENCE!')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE!')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 1:  # Computador jogou PAPEL
+    if jogador == 0:
+        print('COMPUTADOR VENCE!')
+    elif jogador == 1:
+        print('EMPATE!')
+    elif jogador == 2:
+        print('JOGADOR VENCE!')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 2:  # Computador jogou TESOURA
+    if jogador == 0:
+        print('JOGADOR VENCE!')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE!')
+    elif jogador == 2:
+        print('EMPATE!')
+    else:
+        print('JOGADA INVÁLIDA!')
