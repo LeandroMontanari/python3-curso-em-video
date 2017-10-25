c089d1efeffe0e4bc31a949421f15d92d1127d53
LeandroMontanari <otter.lontra@hotmail.com>
2017-10-25 19:32:43 -0200

    Adição do Exercício 047

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 047.py
@@ -0,0 +1,8 @@
+"""
+EXERCÍCIO 047: Contagem de Pares
+
+Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+"""
+for n in range(2, 51, 2):
+    print(n, end=' ')
+print('Acabou')
