611644e44865b0d5c4f6152d6f2454e0fff1f7f4
LeandroMontanari <otter.lontra@hotmail.com>
2017-10-27 19:07:59 -0200

    Adição do Exercício 049

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 049.py
@@ -0,0 +1,9 @@
+"""
+EXERCÍCIO 049: Tabuada v2.0
+
+Refaça o DESAFIO 009, mostrando a tabuada de um número que
+o usuário escolher, só que agora utilizando um laço for.
+"""
+num = int(input('Digite um número para ver sua tabuada: '))
+for c in range(1, 11):
+    print('{} x {:2} = {}'.format(num, c, num * c))
