c1a16a981be2e46a3184e96ff5b4e063f64ae016
LeandroMontanari <otter.lontra@hotmail.com>
2017-11-05 19:20:22 -0200

    Desafio 053 v2.0

--- Mundo 02/Anotações das Aulas e Desafios/Aula 02 - 13/Desafio 053.py
+++ Mundo 02/Anotações das Aulas e Desafios/Aula 02 - 13/Desafio 053.py
@@ -1,53 +1,16 @@
-substituicoes = {
-    'á': 'a',
-    'à': 'a',
-    'ã': 'a',
-    'â': 'a',
-    'é': 'e',
-    'ê': 'e',
-    'í': 'i',
-    'ó': 'o',
-    'õ': 'o',
-    'ô': 'o',
-    'ú': 'u',
-    'ü': 'u',
-    'ç': 'c',
-    '"': '',
-    "'": "",
-    ',': '',
-    '.': '',
-    '!': '',
-    ';': '',
-    ':': '',
-    '?': '',
-    '-': '',
-    '_': '',
-    '(': '',
-    ')': '',
-    'º': '',
-    '<': '',
-    '>': '',
-    '@': '',
-    '#': '',
-    '$': '',
-    '%': '',
-    '*': '',
-    '+': '',
-    '=': '',
-    '[': '',
-    ']': '',
-    '{': '',
-    '}': '',
-    '&': '',
-    '/': '',
-    '\\': ''
-    }
+acentos = {'á': 'a', 'à': 'a', 'ã': 'a', 'â': 'a', 'é': 'e', 'ê': 'e', 'í': 'i',
+           'ó': 'o', 'õ': 'o', 'ô': 'o', 'ú': 'u', 'ü': 'u', 'ç': 'c', "'": ""}
+
+outros = '"´`~^¨,.!;:?-_()[]{}<>ºªº¹²³@#$£¢§¬%*+%=&/\\'
 
 frase = str(input('Digite uma frase: ')).replace('"', '').replace("'", "").capitalize()
 frase_final = frase.lower().strip().replace(' ', '')
 
-for caractere in substituicoes:
-    frase_final = frase_final.replace(caractere, substituicoes[caractere])
+for caractere in acentos:
+    frase_final = frase_final.replace(caractere, acentos[caractere])
+
+for caractere_especial in outros:
+    frase_final = frase_final.replace(caractere_especial, '')
 
 frase_final_invertida = frase_final[::-1]
 
