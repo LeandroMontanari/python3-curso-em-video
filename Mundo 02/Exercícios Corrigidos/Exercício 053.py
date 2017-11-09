4b9c6798d8fe2a43d5bb4e501bcb1c981927c910
LeandroMontanari <otter.lontra@hotmail.com>
2017-11-09 20:28:42 -0200

    Adição do Exercício 053

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 053.py
@@ -0,0 +1,29 @@
+"""
+EXERCÍCIO 053: Detector de Palíndromo
+
+Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+"""
+
+"""
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+print('O inverso de {} é {}.'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
+"""
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+print('O inverso de {} é {}.'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
