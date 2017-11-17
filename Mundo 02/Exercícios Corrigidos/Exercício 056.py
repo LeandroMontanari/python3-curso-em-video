78e61772648bd68c6755be528317e7fd7dbbfd6c
LeandroMontanari <otter.lontra@hotmail.com>
2017-11-16 23:24:50 -0200

    Adição do Exercício 056

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 056.py
@@ -0,0 +1,32 @@
+"""
+EXERCÍCIO 056: Analisador Completo
+
+Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre:
+
+- A média de idade do grupo.
+- Qual é o nome do homem mais velho.
+- Quantas mulheres têm menos de 20 anos.
+"""
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+for p in range(1, 5):
+    print('----- {}ª PESSOA -----'.format(p))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+mediaidade = somaidade / 4
+print('A média de idade do grupo é de {} anos.'.format(mediaidade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(totmulher20))
