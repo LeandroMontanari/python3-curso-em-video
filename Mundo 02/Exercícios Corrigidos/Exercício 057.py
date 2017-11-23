604a57531aa303f4ac8ac159f27848f02c44f8c6
Leandro L. Montanari <otter.lontra@hotmail.com>
2017-11-23 17:20:37 -0200

    Adição do Exercício 057

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 057.py
@@ -0,0 +1,10 @@
+"""
+EXERCÍCIO 057: Validação de Dados
+
+Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
+Caso esteja errado, peça a digitação novamente até ter um valor correto.
+"""
+sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso!'.format(sexo))
