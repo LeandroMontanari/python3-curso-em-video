fa7eeb7a02b27aa90cecae471cb108a55ffe1ced
Leandro L. Montanari <otter.lontra@hotmail.com>
2017-11-23 17:24:36 -0200

    Correção do Desafio 057

--- Mundo 02/Anotações das Aulas e Desafios/Aula 03 - 14/Desafio 057.py
+++ Mundo 02/Anotações das Aulas e Desafios/Aula 03 - 14/Desafio 057.py
@@ -1,8 +1,8 @@
-sexo = str(input('Digite o sexo da pessoa [M/F]: ')).upper()
+sexo = str(input('Digite o seu sexo [M/F]: ')).strip().upper()[0]
 while sexo != 'M' and sexo != 'F':
     print('Valor inválido! Digite M para sexo MASCULINO ou F para sexo FEMININO.')
-    sexo = str(input('Digite o sexo da pessoa [M/F]: ')).upper()
+    sexo = str(input('Digite o seu sexo [M/F]: ')).strip().upper()[0]
 if sexo == 'M':
-    print('A pessoa é do sexo MASCULINO!')
+    print('Você do sexo MASCULINO!')
 elif sexo == 'F':
-    print('A pessoa é do sexo FEMININO!')
+    print('Você é do sexo FEMININO!')
