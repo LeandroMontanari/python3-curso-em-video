5582f07af1bff1148e630a32b095e90cbba9a7c6
Leandro L. Montanari <otter.lontra@hotmail.com>
2017-11-29 22:26:30 -0200

    Adição do Exercício 058

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 058.py
@@ -0,0 +1,24 @@
+"""
+EXERCÍCIO 058: Jogo da Adivinhação v2.0
+
+Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10.
+Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos
+palpites foram necessários para vencer.
+"""
+from random import randint
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+print('Acertou com {} tentativas. Parabéns!'.format(palpites))
