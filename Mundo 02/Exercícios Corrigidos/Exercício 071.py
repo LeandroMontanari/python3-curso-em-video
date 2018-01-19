09c5a64a37849be6aebf391841efd06dbfd76ce5
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-01-19 15:30:18 -0200

    Adição do Exercício 071

---
+++ Mundo 02/Exercícios Corrigidos/Exercício 071.py
@@ -0,0 +1,34 @@
+"""
+EXERCÍCIO 071: Simulador de Caixa Eletrônico
+
+Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao
+usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas
+cédulas de cada valor serão entregues.
+
+OBS: Considere que o caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
+"""
+print('=' * 30)
+print(f'{"BANCO CEV":^30}')
+print('=' * 30)
+valor = int(input('Que valor você quer sacar? R$ '))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R$ {ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
