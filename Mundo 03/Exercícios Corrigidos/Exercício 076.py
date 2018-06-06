d91220cd4aa78364e4acc7202591690f2591c4fe
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-06-06 16:16:54 -0300

    Adição do Exercício 076 e Enunciado do Desafio 076

--- Mundo 03/Exercícios Corrigidos/Exercício 076.py
+++ Mundo 03/Exercícios Corrigidos/Exercício 076.py
@@ -1,27 +1,24 @@
 """
-DESAFIO 076: Lista de Preços com Tupla
+EXERCÍCIO 076: Lista de Preços com Tupla
 
 Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços,
 na sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 """
-div = '-' * 40
-produtos = ('Lápis', 1.75,
-            'Borracha', 2.00,
-            'Caderno', 15.90,
-            'Estojo', 25.00,
+listagem = ('Lápis', 1.75,
+            'Borracha', 2,
+            'Caderno', 15.9,
+            'Estojo', 25,
             'Transferidor', 4.20,
             'Compasso', 9.99,
             'Mochila', 120.32,
             'Canetas', 22.30,
             'Livro', 34.90)
-i, f = 0, 1
-tam = len(produtos)
-
-print(div)
+print('-' * 40)
 print(f'{"LISTAGEM DE PREÇOS":^40}')
-print(div)
-while f < tam:
-    print(f'{produtos[i]:.<31}R$ {produtos[f]:>6.2f}', end='\n')
-    i += 2
-    f += 2
-print(div)
+print('-' * 40)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-' * 40)
