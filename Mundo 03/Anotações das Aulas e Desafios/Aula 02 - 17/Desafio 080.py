b258b8f4491ca578e2416d3803865056789f640c
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-07-04 19:23:46 -0300

    Adição do Exercício 080 e Enunciado do Desafio 080

--- Mundo 03/Anotações das Aulas e Desafios/Aula 02 - 17/Desafio 080.py
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 02 - 17/Desafio 080.py
@@ -1,8 +1,10 @@
 """
-DESAFIO 080
+DESAFIO 080: Lista Ordenada sem Repetições
+
+Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista,
+já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 """
 lista = list()
-
 for n in range(1, 5 + 1):
     valor = int(input('Digite um valor: '))
     if n == 1 or valor > lista[-1]:
---
+++ Mundo 03/Exercícios Corrigidos/Exercício 080.py
@@ -0,0 +1,22 @@
+"""
+EXERCÍCIO 080: Lista Ordenada sem Repetições
+
+Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista,
+já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+"""
+lista = []
+for c in range(0, 5):
+    n = int(input('Digite um valor: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+        print('Adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Adicionado na posição {pos} da lista...')
+                break
+            pos += 1
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram {lista}')
