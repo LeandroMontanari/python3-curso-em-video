12c43bbcb77dae6b02fc46be9b9c25041edc63b6
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-08-07 20:58:14 -0300

    Adição do Exercício 087 e Enunciado do Desafio 087

--- Mundo 03/Anotações das Aulas e Desafios/Aula 03 - 17 (Parte 2)/Desafio 087.py
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 03 - 17 (Parte 2)/Desafio 087.py
@@ -1,5 +1,16 @@
 """
-DESAFIO 087
+DESAFIO 087: Mais Sobre Matriz em Python
+
+Aprimore o desafio anterior, mostrando no final:
+
+A) A soma de todos os valores pares digitados.
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha.
+
+0 [_][_][_]
+1 [_][_][_]
+2 [_][_][_]
+   0  1  2
 """
 matriz = [
     [],
---
+++ Mundo 03/Exercícios Corrigidos/Exercício 087.py
@@ -0,0 +1,37 @@
+"""
+EXERCÍCIO 087: Mais Sobre Matriz em Python
+
+Aprimore o desafio anterior, mostrando no final:
+
+A) A soma de todos os valores pares digitados.
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha.
+
+0 [_][_][_]
+1 [_][_][_]
+2 [_][_][_]
+   0  1  2
+"""
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spar = mai = scol = 0
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 2 == 0:
+            spar += matriz[l][c]
+    print()
+print('-=' * 30)
+print(f'A soma dos valores pares é {spar}.')
+for l in range(0, 3):
+    scol += matriz[l][2]
+print(f'A soma dos valores da terceira coluna é {scol}.')
+for c in range(0, 3):
+    if c == 0:
+        mai = matriz[1][c]
+    elif matriz[1][c] > mai:
+        mai = matriz[1][c]
+print(f'O maior valor da segunda linha é {mai}.')
