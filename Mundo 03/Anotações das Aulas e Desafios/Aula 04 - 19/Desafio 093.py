e2acb6de64e48a6c904c7408904f3157782e1796
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-08-19 19:03:02 -0300

    Adição do Desafio 093

---
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 04 - 19/Desafio 093.py
@@ -0,0 +1,33 @@
+"""
+DESAFIO 093
+"""
+jogador = dict()
+qtdgols = list()
+totgols = 0
+div = '-=' * 30
+jogador['nome'] = str(input('Nome do Jogador: '))
+partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for p in range(partidas):
+    gol = int(input(f'Quantos gols na partida {p + 1}? '))
+    qtdgols.append(gol)
+    totgols += gol
+jogador['gols'] = qtdgols
+jogador['total'] = totgols
+print(div)
+print(jogador)
+print(div)
+for k, v in jogador.items():
+    print(f'O campo {k.capitalize()} tem o valor {v}.')
+print(div)
+print(f'O jogador {jogador["nome"]} jogou {partidas} partidas.')
+for i, v in enumerate(jogador['gols']):
+    if v == 1:
+        leg = 'gol'
+    else:
+        leg = 'gols'
+    print(f'    => Na partida {i + 1}, fez {v} {leg}.')
+if jogador["total"] == 1:
+    leg2 = 'gol'
+else:
+    leg2 = 'gols'
+print(f'Foi um total de {jogador["total"]} {leg2}.')
