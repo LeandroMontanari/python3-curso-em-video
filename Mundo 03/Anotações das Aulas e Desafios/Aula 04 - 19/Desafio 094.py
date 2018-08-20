61ce52458b2b030e9e411586c294d85957f5cfb6
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-08-20 16:29:29 -0300

    Adição do Desafio 094

---
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 04 - 19/Desafio 094.py
@@ -0,0 +1,36 @@
+"""
+DESAFIO 094
+"""
+pessoa = dict()
+pessoas = list()
+media = 0
+while True:
+    pessoa['nome'] = str(input('Nome: '))
+    pessoa['sexo'] = str(input('Sexo [M/F]: ')).upper().strip()[0]
+    pessoa['idade'] = int(input('Idade: '))
+    media += pessoa['idade']
+    pessoas.append(pessoa.copy())
+    continuar = str(input('Quer continuar [S/N]? ')).upper().strip()[0]
+    if 'N' in continuar:
+        break
+print('-=' * 30)
+if len(pessoas) == 1:
+    leg1 = 'pessoa'
+else:
+    leg1 = 'pessoas'
+print(f'- O grupo tem {len(pessoas)} {leg1}.')
+print(f'- A média de idade é de {media / len(pessoas):.2f} anos.')
+print('- As mulheres cadastradas foram: ', end='')
+for p in pessoas:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]}', end=' ')
+print()
+print('- Lista das pessoas que estão acima da média: ')
+for pes in pessoas:
+    if pes['idade'] > media / len(pessoas):
+        print()
+        for k, v in pes.items():
+            print(f'{k.capitalize()} = {v};', end=' ')
+print()
+print()
+print('<< ENCERRADO >>')
