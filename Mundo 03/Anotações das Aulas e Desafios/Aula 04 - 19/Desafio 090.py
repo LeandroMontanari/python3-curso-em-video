dfb2fbacd25cb1f407b5b8e878fcd3c5469685af
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-08-23 17:45:52 -0300

    Adição do Exercício 090 e Enunciado do Desafio 090

--- Mundo 03/Anotações das Aulas e Desafios/Aula 04 - 19/Desafio 090.py
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 04 - 19/Desafio 090.py
@@ -1,5 +1,8 @@
 """
-DESAFIO 090
+DESAFIO 090: Dicionário em Python
+
+Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+No final, mostre o conteúdo da estrutura na tela.
 """
 aluno = dict()
 aluno['Nome'] = str(input('Nome: '))
---
+++ Mundo 03/Exercícios Corrigidos/Exercício 090.py
@@ -0,0 +1,18 @@
+"""
+EXERCÍCIO 090: Dicionário em Python
+
+Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+No final, mostre o conteúdo da estrutura na tela.
+"""
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['média'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
