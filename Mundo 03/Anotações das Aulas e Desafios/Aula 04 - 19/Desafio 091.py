9762ff518c868aceccd06a9673632458c8f4fc43
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-09-03 17:01:42 -0300

    Adição do Exercício 091 e Enunciado do Desafio 091

--- Mundo 03/Anotações das Aulas e Desafios/Aula 04 - 19/Desafio 091.py
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 04 - 19/Desafio 091.py
@@ -1,5 +1,8 @@
 """
-DESAFIO 091
+DESAFIO 091: Jogo de Dados em Python
+
+Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
+dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 """
 from random import randint
 from time import sleep
---
+++ Mundo 03/Exercícios Corrigidos/Exercício 091.py
@@ -0,0 +1,24 @@
+"""
+EXERCÍCIO 091: Jogo de Dados em Python
+
+Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
+dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+"""
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogo = {'jogador1': randint(1, 6),
+        'jogador2': randint(1, 6),
+        'jogador3': randint(1, 6),
+        'jogador4': randint(1, 6)}
+ranking = list()
+print('Valores sorteados:')
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('-=' * 30)
+print('  == RANKING DOS JOGADORES ==')
+for i, v in enumerate(ranking):
+    print(f'   {i + 1}º lugar: {v[0]} com {v[1]}.')
+    sleep(1)
