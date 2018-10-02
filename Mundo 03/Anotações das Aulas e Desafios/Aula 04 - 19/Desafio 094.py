bff9a853c0e3dcd2654e44a132a9086233b2c6e1
Leandro L. Montanari <otter.lontra@hotmail.com>
2018-10-02 18:23:56 -0300

    Adição do Exercício 094 e Enunciado do Desafio 094

--- Mundo 03/Anotações das Aulas e Desafios/Aula 04 - 19/Desafio 094.py
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 04 - 19/Desafio 094.py
@@ -1,5 +1,13 @@
 """
-DESAFIO 094
+DESAFIO 094: Unindo Dicionários e Listas
+
+Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de
+cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:
+
+A) Quantas pessoas cadastradas.
+B) A média de idade.
+C) Uma lista com mulheres.
+D) Uma lista com idade acima da média.
 """
 pessoa = dict()
 pessoas = list()
---
+++ Mundo 03/Exercícios Corrigidos/Exercício 094.py
@@ -0,0 +1,49 @@
+"""
+EXERCÍCIO 094: Unindo Dicionários e Listas
+
+Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de
+cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:
+
+A) Quantas pessoas cadastradas.
+B) A média de idade.
+C) Uma lista com mulheres.
+D) Uma lista com idade acima da média.
+"""
+galera = list()
+pessoa = dict()
+soma = media = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo [M/F]: ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar [S/N]? ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+media = soma / len(galera)
+print(f'B) A média de idade é de {media:5.2f} anos.')
+print('C) As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]} ', end='')
+print()
+print('D) Lista das pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('    ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
